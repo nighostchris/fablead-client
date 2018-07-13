409008bbcd927db796c565df89d1e9624a6bfba7
Chris <chrisliu@joyaether.com>
2018-07-13 12:34:42 +0800

    Dynamic mapping added in teacher page

--- src/Dashboard/TeacherPage.jsx
+++ src/Dashboard/TeacherPage.jsx
@@ -8,74 +8,73 @@ import AddIcon from '@material-ui/icons/Add';
 import FooterBar from '../FooterBar';
 
 const styles = theme => ({
-    teacherWrapper: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: '10px'
-    },
-    teacherCard: {
-        width: '700px',
-        textAlign: 'center',
-        height: '50px',
-        '&:nth-of-type(odd)': {
-            backgroundColor: theme.palette.background.default
-        }
-    },
-    createButton: {
-        float: 'right',
-        marginTop: '40px',
-        marginRight: '20%',
-        [theme.breakpoints.down('sm')]: {
-            display: 'none'
-        }
-    },
-    addButton: {
-        position: 'absolute',
-        bottom: '72px',
-        right: '32px',
-        [theme.breakpoints.up('md')]: {
-          display: 'none'
-        }
+  teacherWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '10px'
+  },
+  teacherCard: {
+    width: '700px',
+    textAlign: 'center',
+    height: '50px',
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default
     }
+  },
+  createButton: {
+    float: 'right',
+    marginTop: '40px',
+    marginRight: '20%',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none'
+    }
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: '72px',
+    right: '32px',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
+  }
 });
 
 class TeacherPage extends React.Component{
-   render(){
-        const { classes } = this.props;
+  render(){
+    const { classes } = this.props;
 
-        return(
-            <div>
-                <div className={ classes.teacherWrapper }>
-                    <List component="nav">
-                        <ListItem button className={ classes.teacherCard }>
-                            <ListItemText primary="Chan Li Li" />
-                        </ListItem>
-                        <ListItem button className={ classes.teacherCard }>
-                            <ListItemText primary="Yuen Ka Yan" />
-                        </ListItem>
-                        <ListItem button className={ classes.teacherCard }>
-                            <ListItemText primary="Wong Man Man" />
-                        </ListItem>
-                        <ListItem button className={ classes.teacherCard }>
-                            <ListItemText primary="Sze Lai Yu" />
-                        </ListItem>
-                    </List>
-                </div>
-                <Button className={ classes.createButton } variant="contained" color="secondary">
+    const data = ["Chan Li Li", "Yuen Ka Yan", "Wong Man Man", "Sze Lai Yu"];
+
+    return(
+      <div>
+        <div className={ classes.teacherWrapper }>
+          <List component="nav">
+            {
+              data.map((data, i) => {
+                return (
+                  <ListItem key={i} button className={ classes.teacherCard }>
+                    <ListItemText primary={ data } />
+                  </ListItem>
+                );
+              })
+            }
+          </List>
+        </div>
+        <Button className={ classes.createButton } variant="contained" color="secondary">
                     Create
-                </Button>
-                <Button color="secondary" className={ classes.addButton } variant="fab" aria-label="help">
-                    <AddIcon />
-                </Button>
-                <FooterBar bottomValue = {2} />
-            </div>
-        );
-    }
+        </Button>
+        <Button color="secondary" className={ classes.addButton } variant="fab" aria-label="help">
+          <AddIcon />
+        </Button>
+        <FooterBar bottomValue = {2} />
+      </div>
+    );
+  }
 }
 
 TeacherPage.propTypes = {
-   classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(TeacherPage);
