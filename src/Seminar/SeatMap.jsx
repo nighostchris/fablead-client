8800c93b61356bfbbe7d1ccd5152296800124d49
Chris <chrisliu@joyaether.com>
2018-07-20 12:57:52 +0800

    Added expansion panel

--- src/Seminar/SeatMap.jsx
+++ src/Seminar/SeatMap.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import HeaderBar from '../HeaderBar';
 
 const styles = theme => ({
+  title: {
+    paddingTop: '10px',
+    marginBottom: '5px',
+    fontWeight: 'bold',
+    fontSize: '16px',
+  },
   seatmapWrapper: {
     textAlign: 'center',
+    backgroundColor: 'white',
   },
   seatmap: {
   },
@@ -30,11 +41,32 @@ const styles = theme => ({
     border: '1px solid black',
     padding: '5px 20px',
   },
+  expansionWrapper: {
+    marginTop: '10px',
+  },
+  heading: {
+    fontSize: '18px',
+    marginLeft: '15px',
+  },
 });
 
 class SeatMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: null,
+    };
+  }
+
+  handleChange = panel => (event, expanded) => {
+    this.setState({
+      expanded: expanded ? panel : false,
+    });
+  };
+
   render() {
     const { classes } = this.props;
+    const { expanded } = this.state;
 
     const rowNumber = 9;
     const distribution = [2, 2, 2, 2];
@@ -81,10 +113,38 @@ class SeatMap extends React.Component {
       <div className={classes.root}>
         <HeaderBar />
         <div className={classes.seatmapWrapper}>
-          <p>1+2號廳 三投2人 座位圖 (143人)</p>
+          <Typography variant="body1" className={classes.title}>1+2號廳 三投2人 座位圖 (143人)</Typography>
           <div className={classes.seatmap}>
             {row}
           </div>
+        </div>
+        <div className={classes.expansionWrapper}>
+          <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography className={classes.heading}>EHE(3)</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+                maximus est, id dignissim quam.
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel expanded={expanded === 'panel2'} onChange={this.handleChange('panel2')}>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography className={classes.heading}>MsShe(5)</Typography>
+            </ExpansionPanelSummary>
+          </ExpansionPanel>
+          <ExpansionPanel expanded={expanded === 'panel3'} onChange={this.handleChange('panel3')}>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography className={classes.heading}>V-MEN(2)</Typography>
+            </ExpansionPanelSummary>
+          </ExpansionPanel>
+          <ExpansionPanel expanded={expanded === 'panel4'} onChange={this.handleChange('panel4')}>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography className={classes.heading}>ZUEE(3)</Typography>
+            </ExpansionPanelSummary>
+          </ExpansionPanel>
         </div>
       </div>
     );
